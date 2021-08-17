5f0d996f2cee636005aa94070eb10ffed66044ea
Swaraj Reddy Chada <swarajreddy10972@gmail.com>
2021-08-17 15:26:05 +0200

    Create utils.py

---
+++ src/Untargeted Attacks/utils.py
@@ -0,0 +1,51 @@
+import torch
+import numpy as np
+
+
+def decode_segmap(image, nc=21): # visualise the decoded prediction (pred)
+  
+  label_colors = np.array([(0, 0, 0),
+               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+  r = torch.zeros_like(image, dtype = torch.uint8)
+  g = torch.zeros_like(image, dtype = torch.uint8)
+  b = torch.zeros_like(image, dtype = torch.uint8)
+  
+  for l in range(0, nc):
+    idx = image == l
+    r[idx] = label_colors[l, 0]
+    g[idx] = label_colors[l, 1]
+    b[idx] = label_colors[l, 2]
+    
+  rgb = torch.stack([r, g, b], axis=3)
+  return rgb
+
+def IoUAcc(y_trg, y_pred, class_names = class_names):
+
+    trg = y_trg.squeeze(1)
+    pred = y_pred
+    iou_list = list()
+    present_iou_list = list()
+
+    pred = pred.view(-1)
+    trg = trg.view(-1)
+    for sem_class in range(21): # loop over each class for IoU calculation
+        pred_inds = (pred == sem_class)
+        target_inds = (trg == sem_class)
+        if target_inds.long().sum().item() == 0:
+            iou_now = float('nan')
+            #print('Class {} IoU is {}'.format(class_names[sem_class+1], iou_now))
+        else: 
+            intersection_now = (pred_inds[target_inds]).long().sum().item()
+            union_now = pred_inds.long().sum().item() + target_inds.long().sum().item() - intersection_now
+            iou_now = float(intersection_now) / float(union_now)
+            #print('Class {} IoU is {}'.format(class_names[sem_class+1], iou_now))
+            present_iou_list.append(iou_now)
+        iou_list.append(iou_now)
+        
+    acc = accuracy_score(trg, pred)
+    
+    return np.mean(present_iou_list)*100, acc*100
+
